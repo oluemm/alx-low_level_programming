2df9a19c5c3843465e0161910c8d55a882a0ec87
fashemma007 <fasogbaemmanuel@gmail.com>
2022-06-30 19:31:00 +0100

    advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
  *
  * Return: return 1 after execution ==> Standard Error
  */
-int main(void){
+int main(void)
+{
 	wchar_t sr[] = L"and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	wprintf(L"%ls\n", sr);
