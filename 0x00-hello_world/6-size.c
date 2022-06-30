4924c1dbae7b02932796846acd4389f97e86d1a0
fashemma007 <fasogbaemmanuel@gmail.com>
2022-06-30 19:07:53 +0100

    size of types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /*
- *Write a C program that prints the size of various types on 
+ *Write a C program that prints the size of various types on
  *the computer it is compiled and run on.
  *You should produce the exact same output as in the example
  *Warnings are allowed
  *Your program should return 0
- *You might have to install the package libc6-dev-i386 
+ *You might have to install the package libc6-dev-i386
  *on your Linux to test the -m32 gcc option
  */
 int main(void)
