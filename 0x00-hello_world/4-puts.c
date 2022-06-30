4d46c8a4b4a53c7801e72a8b290fb231c86b80d2
fashemma007 <fasogbaemmanuel@gmail.com>
2022-06-30 19:18:44 +0100

    using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 /**
-*Write a C program that prints exactly
-*"Programming is like building a multilingual puzzle,
-*followed by a new line
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ * Write a C program that prints exactly
+ * "Programming is like building a multilingual puzzle,
+ * followed by a new line
+ */
 
 int main(void)
 	{
