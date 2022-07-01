5fea0d41afe46c017c098d30073cd8e95e8b5944
fashemma007 <fasogbaemmanuel@gmail.com>
2022-07-01 19:01:19 +0100

    using ascii codes to print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * This program prints nos from 0 to 10
+ * Return: Always 0 (success/corect)
+ */
+
+int main(void)
+{
+	int num;
+	for (num = 48 ; num <= 57; num++)
+	{
+		putchar(num);
+	}
+	putchar ('\n');
+	return (0);
+}
