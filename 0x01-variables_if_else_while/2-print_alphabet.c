ab2ac151ac549ac6d547cbdf3aea9819a63ff56a
fashemma007 <fasogbaemmanuel@gmail.com>
2022-07-01 17:52:32 +0100

    la-alphabeta

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,15 +9,13 @@
 int main(void)
 {
     char alphabets;
-    for (
-        alphabets = 'a';
-        alphabets <= 'z';
-        ++alphabets
-        )
+    
+    for (alphabets = 'a'; alphabets <= 'z';alphabets++)
     {
-	    putchar(alphabets);
-    }	
-    putchar("\n");
+        putchar(alphabets);
+    }
+    
+    putchar('\n');
     return (0);
 }
 
