35833bf0af8521d5118daf04fd401700dff4e6e1
fashemma007 <fasogbaemmanuel@gmail.com>
2022-07-01 20:16:27 +0100

    advanced tasks

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,23 +8,23 @@
 int main(void)
 
 {
-	int number_left;
-	int number_right;
+int number_left;
+int number_right;
 
-	for (number_left = 48; number_right <= 57; number_left++)
+for (number_left = 48; number_right <= 57; number_left++)
+{
+for (number_right = number_left + 1 ; number_right <= 57; number_right++)
+{
+	putchar(number_left);
+	putchar(number_right);
+	if ((number_left == 57) && (number_right == 57))
 	{
-		for (number_right = number_left + 1 ; number_right <= 57; number_right++)
-		{
-			putchar(number_left);
-			putchar(number_right);
-			if ((number_left == 56) && (number_right == number_left + 1))
-			{
-				break;
-			}
-			putchar(',');
-			putchar (' ');
-		}
-	}
-	putchar('\n');
-	return (0);
+		break;
+}
+putchar(',');
+putchar (' ');
+}
+}
+putchar('\n');
+return (0);
 }
