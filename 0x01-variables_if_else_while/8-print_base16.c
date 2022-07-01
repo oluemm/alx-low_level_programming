d90c1909f36dae7f2a8e10c0b6d12d15f167540d
fashemma007 <fasogbaemmanuel@gmail.com>
2022-07-01 19:30:53 +0100

    using asci values to print hexadecimal numbers

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,11 +8,13 @@ int main(void)
 {
 	char base_16;
 	char nums;
-	
+
+
 	for (nums = 48; nums <= 57; nums++)
 	{
 		putchar(nums);
 	}
+	
 	for (base_16 = 97 ; base_16 <= 102; base_16++)
 	{
 		putchar(base_16);
