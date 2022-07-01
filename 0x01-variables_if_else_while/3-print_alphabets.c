f6cc0386d1ca5ba739fda46bd9dadba082ee7cbf
fashemma007 <fasogbaemmanuel@gmail.com>
2022-07-01 18:10:43 +0100

    la-alphaBETA

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,28 +1,25 @@
 #include <stdio.h>
 /**
- * main : Program entry point
- *
- *Program prints out the alphabets in lower case
- *
+ * main -Program entry point
+ * Program prints out the alphabets in lower case
+ * and then UPPER CASE
+ * Return: 0 on success, 1 on error
  */
 
-int main() {
-    char c;
-    char b;
-    for (
-        c = 'a'; 
-        c <= 'z'; 
-        ++c
-        )
-        putchar(c);
-    for (
-        b = 'A'; 
-        b <= 'Z'; 
-        ++b
-        )
-        
-        putchar(b);
-    putchar("\n");
-    return 0;
+int main(void)
+{
+char alphabets;
+char ALPHABETS;
+
+for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+{
+putchar(alphabets);
+}
+for (ALPHABETS = 'A'; ALPHABETS <= 'Z'; ALPHABETS++)
+{
+putchar(ALPHABETS);
+}
+putchar('\n');
+return (0);
 }
 
