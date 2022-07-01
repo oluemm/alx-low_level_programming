54563c383ec7ca4273049dea2eb71f88e418ee16
fashemma007 <fasogbaemmanuel@gmail.com>
2022-07-01 14:55:00 +0100

    la-alphaBETA

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main : Program entry point
+ *
+ *Program prints out the alphabets in lower case
+ *
+ */
+
+int main() {
+    char c;
+    char b;
+    for (
+        c = 'a'; 
+        c <= 'z'; 
+        ++c
+        )
+        putchar(c);
+    for (
+        b = 'A'; 
+        b <= 'Z'; 
+        ++b
+        )
+        
+        putchar(b);
+    return 0;
+}
+
