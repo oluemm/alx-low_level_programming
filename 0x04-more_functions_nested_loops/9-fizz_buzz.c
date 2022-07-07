398fe4efb6be8fa5938dc0c4710ff5ea4c1eec4f
fashemma007 <fasogbaemmanuel@gmail.com>
2022-07-07 19:09:40 +0100

    Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -22,11 +22,18 @@ int main(void)
 		}
 		else if ((num % 5) == 0)
 		{
-			printf("Buzz ");
+			if (num == 100)
+			{
+				printf("Buzz");
+			}
+			else
+			{
+				printf("Buzz ");
+			}
 		}
 		else
 		{
-			printf("%d ", num);
+			printf("%i ", num);
 		}
 	}
 	printf("\n");
