9d4e556e91083351d4096bad175cd3e9e4577a4a
fashemma007 <fasogbaemmanuel@gmail.com>
2022-07-07 13:03:41 +0100

    function that multiplies 2 integers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,10 +4,12 @@
  * @a: variable 1
  * @b: variable 2
  * This program takes in two variables and returns their product
+ * Return: product
  */
 int mul(int a, int b)
 {
 	int mul;
+
 	mul = a * b;
 	return (mul);
 }
