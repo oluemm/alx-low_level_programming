2f5a0ab6452e9f2aeeecfb37cd34eec7abac22bc
fashemma007 <fasogbaemmanuel@gmail.com>
2022-07-07 13:14:16 +0100

    prints the numbers, from 0 to 9, followed by a new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - Starts here
+ * prints numbers from 0 - 9 and adds a new line
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+	
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 int _putchar(char c);
+void print_numbers(void);
 int mul(int a, int b);
 int _isupper(int c);
 int _isdigit(int c);
