75daeba9e6ad3d4464d55a209a9c70a172d5961e
fashemma007 <fasogbaemmanuel@gmail.com>
2022-07-07 11:42:48 +0100

    function that checks charach case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * program checks alphabets for uppercase
+ * Returns 1 if upper and 0 is not
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90 )
+	{
+		return(1);
+	}
+	else
+		return (0);
+}
