e2cc1ab4bcdd3570efd541535946cc37fac9a06c
fashemma007 <fasogbaemmanuel@gmail.com>
2022-07-07 12:22:07 +0100

    function that checks if args is a digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- * main - Entry point
+ * _isdigit - program begins here
+ * @c: input
  * program checks for digits
- * Returns 1 if it is and 0 is not
+ * Return: 1 if it is and 0 is not
  */
 
 int _isdigit(int c)
 {
-	if (c >= 48 && c <= 57 )
+	if (c >= 48 && c <= 57)
 	{
-		return(1);
+		return (1);
 	}
 	else
 		return (0);
