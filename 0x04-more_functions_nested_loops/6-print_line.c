0e26e5a1f15ccf0370ff5998aa4849e832bbcb93
fashemma007 <fasogbaemmanuel@gmail.com>
2022-07-07 14:47:16 +0100

    prints '_' and adds a new line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - starts
+ * @n: input
+ */
+
+void print_line(int n)
+{
+	int a,k;
+	k = 95;
+
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+		{
+			_putchar(k);
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 int _putchar(char c);
-
+void print_line(int n);
 void more_numbers(void);
 void print_most_numbers(void);
 void print_numbers(void);
