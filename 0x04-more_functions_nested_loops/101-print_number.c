ef21b143e1244ee4f21c1bb594c0d4e4c8099625
fashemma007 <fasogbaemmanuel@gmail.com>
2022-07-08 03:43:19 +0100

    print complex integers with constraints

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,14 +5,14 @@
  */
 void print_number(int n)
 {
-	short len, val, itr, exp, cache;
+	int len, val, itr, exp, cache;
 
 	val = n;
 	exp = len = 1;
 /*Check for negative values*/
 	if (val < 0)
 	{
-		val *= -1;
+		val = -1 * val;
 		_putchar('-');
 	}
 	cache = val;
@@ -23,12 +23,12 @@ void print_number(int n)
 	}
 	for (itr = 1; itr < len; itr++)
 	{
-		exp *= 10;
+		exp = exp * 10;
 	}
 	while (exp > 1)
 	{
 		_putchar((val / exp) % 10 + '0');
-		exp /= 10;
+		exp = exp / 10;
 	}
 	_putchar(val % 10 + '0');
 }
