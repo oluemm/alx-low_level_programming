3631940a43e49732d65b2e86a86cec8951d0657c
fashemma007 <fasogbaemmanuel@gmail.com>
2022-07-12 13:13:40 +0100

    style:formatted code to fit betty-style

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * reset_to_98 - Function takes a pointer as parameter 
+ * reset_to_98 - Function takes a pointer as parameter
  * and updates it's value to 98.
  * @n:pointer input
  */
