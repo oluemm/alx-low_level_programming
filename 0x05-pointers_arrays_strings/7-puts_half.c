2062525cce8dff4cafbb0c7c245e9d65c6a09f3b
fashemma007 <fasogbaemmanuel@gmail.com>
2022-07-12 15:50:41 +0100

    feat: A function that prints half of a string, followed by a new line.

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,22 +1,23 @@
 #include "main.h"
 /**
- * puts_half - Prints out the first half of a string.
- * @str: input string to print.
+ *puts_half - prints every other character of a string
+ *@str: A pointer to an int that will be changed
+ *
+ *Return: void which means our answer is correct
  */
 void puts_half(char *str)
 {
-	int i, j;
+	int i, last;
 
+	i = 0;
 	while (str[i] != '\0')
-		i++;
-	if (i % 2 == 0)
-		j = i / 2;
-	else
-		j = (i + 1) / 2;
-	while (j < i)
 	{
-		_putchar(str[j]);
-		j++;
+		i++;
 	}
-	_putchar('\n');
+	last = (i + 1) / 2;
+	for (i = last; str[i]; i++)
+	{
+		_putchar (str[i]);
+	}
+	_putchar ('\n');
 }
