c848d84f07334867fbc0c74ded905aabddc9cd19
fashemma007 <fasogbaemmanuel@gmail.com>
2022-07-14 21:00:25 +0100

    func that concatenates 2 strings reloaded

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,16 +8,15 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-    int i, j;/* i is the index of dest, j is the index of src */
-
-    for (i = 0; dest[i] != '\0'; i++)/*run the index of i to the end of string*/
-    {
-    }
-    for (j = 0; j < n && src[j] != '\0'; j++)/*initialize a for loop*/
-    {
-        dest[i] = src[j];/*append the values of src to dest*/
-        i++;/*increment i*/
-    }
-    dest[i] = '\0';/*append the null character to the end of the string*/
-    return (dest);
+	int i, j;/* i is the index of dest, j is the index of src */
+	for (i = 0; dest[i] != '\0'; i++)/*run the index of i to the end of string*/
+	{
+	}
+	for (j = 0; j < n && src[j] != '\0'; j++)/*initialize a for loop*/
+	{
+		dest[i] = src[j];/*append the values of src to dest*/
+		i++;/*increment i*/
+	}
+	dest[i] = '\0';/*append the null character to the end of the string*/
+	return (dest);
 }
