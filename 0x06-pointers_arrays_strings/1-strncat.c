673d2c30fad751a61e79ffedf4efb83eaa87b033
fashemma007 <fasogbaemmanuel@gmail.com>
2022-07-14 21:05:18 +0100

    func that concatenates 2 strings reloaded

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
- * _strcat - Concatenates 2 strings
+ * _strncat - Concatenates 2 strings
  * @dest: Destination string
  * @src: Source string
- * n: Number of bytes to copy
+ * @n: Number of bytes to copy
  * Return: Returns the concatenated string.
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;/* i is the index of dest, j is the index of src */
+
 	for (i = 0; dest[i] != '\0'; i++)/*run the index of i to the end of string*/
 	{
 	}
