ab5d5116bb91ed243261112fcb4bd387e77d549c
fashemma007 <fasogbaemmanuel@gmail.com>
2022-07-19 14:03:16 +0100

    function that sets the value of a pointer to a char.

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: default char pointer
+ * @to: value to be repointed to
+ */
+ void set_string(char **s, char *to)
+{
+	*s = to;
+}
