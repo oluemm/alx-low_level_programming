be7612daa98a2a4ebad22b937be239b9b405f0e8
fashemma007 <fasogbaemmanuel@gmail.com>
2022-07-20 16:08:22 +0100

    a function that prints a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: string passed in
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{/*if the pointer isn't a null byte*/
+		_print_rev_recursion(s + 1);
+		/*call function and move cursor one step forward*/
+		_putchar(*s);/*print out the last character*/
+	}
+	else
+	{
+		_putchar('\0');/*print out end of string i.e null byte*/
+	}
+}
