2e614c234a0d930f8e148f7f181d44cfa96a091a
fashemma007 <fasogbaemmanuel@gmail.com>
2022-07-28 12:54:05 +0100

    compulsory

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,15 +9,19 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *s;
+	int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-
 	s = malloc(nmemb * size);
 	if (s == NULL)
 	{
 		free(s);
 		return (NULL);
+	}
+	for (i = 0; i < nmemb; i++) /*loop thru array; stop at user defined point*/
+	{
+		s[i] = 0; /*dereferencing each given value of the array*/
 	}
 	return (s);
 }
