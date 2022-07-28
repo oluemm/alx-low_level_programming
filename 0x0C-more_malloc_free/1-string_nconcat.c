7e933c98192adfa9f2a04716a5f042d0c5366ef5
fashemma007 <fasogbaemmanuel@gmail.com>
2022-07-28 12:32:13 +0100

    compulsory

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,40 +12,22 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int str1 = 0, str2 = 0, i;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
-
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
-
 	while (s1[str1] != '\0')
-	{
 		str1++;
-	}
 	while (s2[str2] != '\0')
-	{
 		str2++;
-	}
 	if (n > str2)
-	{
 		n = str2;
-		p = malloc((str1 + n + 1) * sizeof(char));
-	}
+	p = malloc((str1 + n + 1) * sizeof(char));
 	if (p == NULL)
-	{
 		return (0);
-	}
 	for (i = 0; i < str1; i++)
-	{
 		p[i] = s1[i];
-	}
 	for (; i < (str1 + n); i++)
-	{
 		p[i] = s2[i - str1];
-	}
 	p[i] = '\0';
 	return (p);
 }
