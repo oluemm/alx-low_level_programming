9cabe236ca2a25743a96afa335a4932ae9997a1e
fashemma007 <fasogbaemmanuel@gmail.com>
2022-08-04 21:12:03 +0100

    a function that prints anything

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,5 +6,9 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char *const format, ...);
-
+typedef struct VTyp
+{
+	char *valid;
+	void (*f)();
+} types;
 #endif /*VARIADIC_FUNCTIONS*/
