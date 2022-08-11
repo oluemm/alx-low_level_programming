bbf586c44c88b414c1ee4e974b63c9a0412c3afc
fashemma007 <fasogbaemmanuel@gmail.com>
2022-08-11 11:25:06 +0100

    print_list: prints all the elements of a list_t list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,8 +13,8 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL) /*check if node is NULL*/
 			printf("[0] (nil) \n");
 		else
-			printf("[%d] %s \n", h->len, h->str); /*print each node  in LL*/
-		h = h->next;
+			printf("[%d] %s \n", h->len, h->str); /*print vals in current node*/
+		h = h->next;							  /*move on to next node*/
 	}
 	return (i);
 }
