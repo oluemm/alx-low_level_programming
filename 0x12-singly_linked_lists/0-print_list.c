84034a034971ca52663a4154f783d017aee0a09f
fashemma007 <fasogbaemmanuel@gmail.com>
2022-08-11 14:46:49 +0100

    print_list: prints all the elements of a list_t list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,16 +2,16 @@
 /**
  * print_list - prints all the elements of a list_t list.
  * @h: pointer to the list_t list.
- * Return: the number of nodes.
+ * Return: the number of nodes/element in list.
  */
 size_t print_list(const list_t *h)
 {
-	size_t i; /*initialize variable i to datatype size_t*/
+	size_t i; /*initialize counter i to datatype size_t*/
 
 	for (i = 0; h != NULL; i++) /*loop thru l-list*/
 	{
 		if (h->str == NULL) /*check if node is NULL*/
-			printf("[0] (nil) \n");
+			printf("[%d] %s \n", 0, "(nil)");
 		else
 			printf("[%d] %s \n", h->len, h->str); /*print vals in current node*/
 		h = h->next;							  /*move on to next node*/
