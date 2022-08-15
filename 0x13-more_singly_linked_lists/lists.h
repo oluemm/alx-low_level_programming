9d4c4d4183a497c2597c6d117011843e0ed0b2a7
fashemma007 <fasogbaemmanuel@gmail.com>
2022-08-15 11:50:49 +0100

    returns the number of elements in a linked listint_t list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -16,11 +16,12 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 /*===================PROTOTYPES=====================*/
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 
 #endif
