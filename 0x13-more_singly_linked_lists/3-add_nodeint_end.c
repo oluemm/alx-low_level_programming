5d90a002bd737ebaf98ce2a48a9ee74aa355381b
fashemma007 <fasogbaemmanuel@gmail.com>
2022-08-15 13:08:28 +0100

    add_nodeint_end - add new nodes to the end of the list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
  * add_nodeint_end - add new nodes to the end of the list
- * @head: current place in the list
- * @n: int to add to the list
+ * @head: current place in the lists
+ * @n: int to add to the lists
  * Return: pointer to current position in list
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
