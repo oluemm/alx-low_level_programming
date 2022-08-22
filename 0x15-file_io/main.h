535ae88b9102c33744cfdf4fb3c56c6a0ef00175
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-08-22 16:10:31 +0100

    reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,31 +1,14 @@
-#include "main.h"
-/**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: pointer to text in a file
- * @letters: number of letters
- * Return: the actual number of letters it could read and print
- */
-ssize_t read_textfile(const char *filename, size_t letters)
-{
-	ssize_t file, fread, fwrite;
-	char *totalSize;
+#ifndef MAIN_H
+#define MAIN_H
 
-	totalSize = malloc(sizeof(char) * letters);
-	if (totalSize == NULL)
-		return (0);
-	if (filename == NULL)
-		return (0);
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-	file = open(filename, O_RDONLY);
-	if (file == -1)
-		return (0);
-	fread = read(file, totalSize, letters);
-	if (fread == -1)
-		return (0);
-	fwrite = write(STDOUT_FILENO, totalSize, fread);
-	if (fwrite == -1)
-		return (0);
-	close(file);
-	free(totalSize);
-	return (fwrite);
-}
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* MAIN_H */
