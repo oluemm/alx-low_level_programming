d63234643fefb85af5113d0de253c49ae435db75
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-10-06 06:47:56 +0100

    md5

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/fashemma007/alx-low_level_programming/main/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp/ https://raw.github.com/fashemma007/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD=/tmp/nrandom.so
